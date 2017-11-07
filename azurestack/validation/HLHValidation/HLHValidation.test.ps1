eb5b0a63c3f2604fd9029c07aa0272b12f827dbd
Eric Marquez <emarq@microsoft.com>
2017-11-07 02:18:14 -0800

    Added VLAN test from HLH.

--- azurestack/validation/HLHValidation/HLHValidation.test.ps1
+++ azurestack/validation/HLHValidation/HLHValidation.test.ps1
@@ -36,24 +36,44 @@ Describe "Network Validation"{
     foreach($i in $item){
         $networkData = get-networkdetails `
             -ConfigurationData (Get-Content -raw $i.FullName | ConvertFrom-Json )
+
+        # get a list of all the IP's assigned in the BMCMgmt hash group.
+        $bmcmgmtAssignments = ($networkdata.IPConfigData | 
+            Where-Object {$_.Name -match "BMCMgmt"}).Assignments
+        $hlhosIPv4 = ($bmcmgmtAssignments | 
+            Where-Object {$_.Name -match "HLH-OS"}).IPv4Address
+
         Context "Test $($networkData.Name) P2P Links"{
             $P2PObjects = $networkdata.IPConfigData | Where-Object {$_.Name -match "P2P"}
             foreach($P2p in $P2PObjects){
-                # get a list of all the IP's assigned in the BMCMgmt hash group.
-                $bmcmgmtAssignments = ($networkdata.IPConfigData | 
-                    Where-Object {$_.Name -match "BMCMgmt"}).Assignments
-                $hlhosIPv4 = ($bmcmgmtAssignments | 
-                    Where-Object {$_.Name -match "HLH-OS"}).IPv4Address
+
                 it "Test-Netconnection from HLH-OS ($hlhosIPv4) to $($p2p.Name) ($($p2p.IPv4FirstAddress))"{
                     (Test-NetConnection $p2p.IPv4FirstAddress).PingSucceeded |
                         Should be $true
-                    
                 }
+
                 it "Test-NetConnect from HLH-OS ($hlhosIPv4) to $($p2p.Name) ( $($P2P.IPv4LastAddress) )"{
                     (Test-NetConnection $P2P.IPv4LastAddress).PingSucceeded |
                         Should be $true
                 }
             }
+        }
+        Context "Test $($networkData.Name) VLANs"{
+            $vlans = $networkData.IPConfigData | Where-Object {$_.SubnetType -eq 'VLAN'} 
+
+            foreach($vlan in $vlans){
+
+                it "Test-NetConnection from HLH-OS ($hlhosIPv4) to $($vlan.Name) ($($vlan.IPv4Gateway))"{
+                    if($vlan.Name -match 'Storage'){
+                        (Test-Netconnection -computerName $vlan.IPv4Gateway).PingSucceeded |
+                            Should be $false
+                    }
+                    else{
+                        (Test-NetConnection -ComputerName $vlan.IPv4Address).PingSucceeded |
+                            Should Be $true
+                    } 
+                }
+            }  
         }
     }
 }
