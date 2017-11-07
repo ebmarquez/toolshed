7f02c49e323cb255914146ea99f6e5e0991da4df
Eric Marquez <emarq@microsoft.com>
2017-11-07 01:42:11 -0800

    P2P Testing added from configurationData

--- azurestack/validation/HLHValidation/HLHValidation.test.ps1
+++ azurestack/validation/HLHValidation/HLHValidation.test.ps1
@@ -18,33 +18,36 @@ function get-networkdetails{
     [CmdletBinding()]
     param($ConfigurationData)
     
-    foreach($cd in $ConfigurationData){
-        $NamingPrefix = $cd.InputData.NamingPrefix
-        $objectDetails = [customepsobject]@{
-            Name = $NamingPrefix
-            IPConfigData = $cd.IPConfigData
-            EnvironmentData = $cd.EnvironmentData
-            InputData = $cd.InputData
-            }
-        Write-Output $objectDetails
-    }
+    $NamingPrefix = $ConfigurationData.InputData.Cloud.NamingPrefix | sort-object| get-unique
+    Write-Verbose -Message "ConfigurationData Name: $namingPrefix"
+    $objectDetails = [pscustomobject]@{
+        Name = $NamingPrefix
+        IPConfigData = $ConfigurationData.IPConfigData
+        EnvironmentData = $ConfigurationData.EnvironmentData
+        InputData = $ConfigurationData.InputData
+        }
+    Write-Output $objectDetails
+
 }
 
-Describe "Test From HLH"{
+Describe "Network Validation"{
     
-    $item = Get-ChildItem C:\results -Filter "ConfigurationData" -Recurse -File
+    $item = Get-ChildItem C:\results\CiscoExample -Filter "ConfigurationData.json" -Recurse -File
     foreach($i in $item){
-        $networkData = get-networkdetails -ConfigurationData (Get-Content -raw $i.FullName | ConvertFrom-Json )
-        
-        Context "Connection Test from HLH to $($networkData.Name) P2P Links"{
+        $networkData = get-networkdetails `
+            -ConfigurationData (Get-Content -raw $i.FullName | ConvertFrom-Json )
+        Context "Test $($networkData.Name) P2P Links"{
             $P2PObjects = $networkdata.IPConfigData | Where-Object {$_.Name -match "P2P"}
             foreach($P2p in $P2PObjects){
                 # get a list of all the IP's assigned in the BMCMgmt hash group.
-                $bmcmgmtAssignments = ($networkdata.IPConfigData | Where-Object {$_.Name -match "BMCMgmt"}).Assignments
-                $hlhosIPv4 = ($bmcmgmtAssignments | Where-Object {$_.Name -match "HLH-OS"}).IPv4Address
+                $bmcmgmtAssignments = ($networkdata.IPConfigData | 
+                    Where-Object {$_.Name -match "BMCMgmt"}).Assignments
+                $hlhosIPv4 = ($bmcmgmtAssignments | 
+                    Where-Object {$_.Name -match "HLH-OS"}).IPv4Address
                 it "Test-Netconnection from HLH-OS ($hlhosIPv4) to $($p2p.Name) ($($p2p.IPv4FirstAddress))"{
                     (Test-NetConnection $p2p.IPv4FirstAddress).PingSucceeded |
                         Should be $true
+                    
                 }
                 it "Test-NetConnect from HLH-OS ($hlhosIPv4) to $($p2p.Name) ( $($P2P.IPv4LastAddress) )"{
                     (Test-NetConnection $P2P.IPv4LastAddress).PingSucceeded |
